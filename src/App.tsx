1326a8ccc5961c5140b0ee0de8614ae23e2efc52
jordanjohnston <jortathlon@hotmail.com>
2022-02-12 16:18:45 +0000

    - some qol improvements, mobile css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import img from './no-bullyaaaaahhh.png';
+import img from './rounded.png';
 import './App.css';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 
 const mult = (2**8);
 
@@ -14,6 +14,14 @@ const App = () => {
   const [textInput, setTextInput] = useState('');
   const [items, setItems] = useState<string[]>([]);
   const [chosen, setChosen] = useState('- no item chosen -');
+
+  const mobileWidth = 900;
+  let imgWidth = 400;
+
+  const screen = { w: window.innerWidth, h: window.innerHeight };
+  
+  if(screen.w < mobileWidth)
+    imgWidth = 200;
 
   const onClick = () => {
     const img = document.getElementById('img');
@@ -34,24 +42,122 @@ const App = () => {
 
   const setRandom = () => {
     const a = Math.random() * items.length * mult;
-    setChosen(items[Math.floor(a) % items.length]);
+    setChosen(`Your Pick -> ${items[Math.floor(a) % items.length]}`);
   };
 
   const onClickAdd = () => {
     if(textInput.length > 0) {
       setItems([...items, textInput]);
       setTextInput('');
+      const input = document.getElementById('text-input');
+      if(input) {
+        input.focus();
+      }
     }
   };
+
+  const onClickClear = () => {
+    setItems([]);
+    setChosen('- no item chosen -');
+  };
+
+  // mobile
+  if(screen.w < mobileWidth) {
+    return <Mobile
+      chosen={chosen}
+      imgWidth={imgWidth}
+      onClick={onClick}
+      onClickAdd={onClickAdd}
+      onClickClear={onClickClear}
+      items={items}
+      textInput={textInput}
+      setTextInput={setTextInput}
+    />;
+  }
+
+  // desktop
+  return <Desktop
+    chosen={chosen}
+    imgWidth={imgWidth}
+    onClick={onClick}
+    onClickAdd={onClickAdd}
+    onClickClear={onClickClear}
+    items={items}
+    textInput={textInput}
+    setTextInput={setTextInput}
+  />;
+};
+
+
+interface Props {
+  chosen: string;
+  imgWidth: number;
+  onClick: () => void;
+  onClickAdd: () => void;
+  onClickClear: () => void;
+  items: string[];
+  textInput: string;
+  setTextInput: (_: string) => void;
+}
+
+const Mobile: FC<Props> = (props) => {
+  const { imgWidth, chosen, onClick, onClickAdd, onClickClear, items, textInput, setTextInput } = props;
+
+  return (
+    <div className="App">
+      <h2>Scuffed Picker</h2>
+      <div className='block'>
+        <div className='block'>
+          <img className='image' id='img' src={img} alt="wheeeeee" width={imgWidth} height={imgWidth} />
+        </div>
+        <div className='text-back'>
+          <p className='bold'>{chosen}</p>
+        </div>
+        <div className='center'>
+          <div>
+            <button className='button-64' onClick={() => onClick()}>
+              <span className='text'>SPIN</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='block'>
+        <div className='text-back'>
+          <h4>Enter your options here:</h4>
+        </div>
+        <div className=''>
+          <input className='text-input' id='text-input' placeholder='Enter Options...' value={textInput} onChange={(e) => setTextInput(e.currentTarget.value)} type='text' />
+          <div className='inline-button'>
+            <button className='button-63' onClick={onClickAdd}>Add</button>
+          </div>
+          <div className='block'>
+            <button className='button-63' onClick={onClickClear}><span>Clear Items</span></button>
+          </div>
+        </div>
+        <div className='block text-back'>
+          <div className='list-items'>
+            <h4>Options:</h4>
+            <ul>
+              {items.map((item, i) => <li key={`${i}_${item}`}>{item}</li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Desktop: FC<Props> = (props) => {
+  const { imgWidth, chosen, onClick, onClickAdd, onClickClear, items, textInput, setTextInput } = props;
 
   return (
     <div className="App">
       <h2>Scuffed Picker</h2>
       <div className='left'>
         <div className="container">
-          <img id='img' src={img} alt="wheeeeee" width={400} height={400} />
+          <img id='img' src={img} alt="wheeeeee" width={imgWidth} height={imgWidth} />
         </div>
-        <p>{chosen}</p>
+        <p className='bold'>{chosen}</p>
         <div className='button-container'>
           <div>
             <button className='button-64' onClick={() => onClick()}>
@@ -61,22 +167,27 @@ const App = () => {
         </div>
       </div>
       <div className='right'>
-        <h4>Enter your shit here:</h4>
-        <div className='block'>
-          <input value={textInput} onChange={(e) => setTextInput(e.currentTarget.value)} type='text' />
-          <button onClick={onClickAdd}><span>Add</span></button>
+        <h4>Enter your options here:</h4>
+        <div className='input-container'>
+          <input className='text-input' id='text-input' placeholder='Enter Options...' value={textInput} onChange={(e) => setTextInput(e.currentTarget.value)} type='text' />
+          <div className='inline-button'>
+            <button className='button-63' onClick={onClickAdd}>Add</button>
+          </div>
+          <div className='block'>
+            <button className='button-63' onClick={onClickClear}><span>Clear Items</span></button>
+          </div>
         </div>
-        <button onClick={() => { setItems([]); setChosen('- no item chosen -'); }}><span>Clear Items</span></button>
-        <div className='block'>
-          <h4>Your shit:</h4>
-          <ul>
-            {items.map((item, i) => <li key={`${i}_${item}`}>{item}</li>)}
-          </ul>
+        <div className='center text-back' style={{ marginTop: 10 }}>
+          <div>
+            <h4>Options:</h4>
+            <ul>
+              {items.map((item, i) => <li key={`${i}_${item}`}>{item}</li>)}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default App;
